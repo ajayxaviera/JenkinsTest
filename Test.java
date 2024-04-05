24780ee1abca26b58f58ca6b6146535649e85351
ajayxaviera <147126399+ajayxaviera@users.noreply.github.com>
2024-04-05 11:48:19 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,11 @@
 public class Test{
   public static void main(String[] args){
     System.out.println("Hello!This is Ajay");
+    Test obj = new Test();
+    obj.myage();
   } 
+
+  public void myage(){
+    System.out.println("My Age is 18");
+  }
 }
